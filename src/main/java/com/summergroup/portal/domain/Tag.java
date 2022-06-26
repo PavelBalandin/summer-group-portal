698dd0857f66ef0a68aa049509d5502bc9abfad8
Pavlo Balandin <pashabalandin1200@gmail.com>
2022-06-26 21:15:11 +0300

    Change list of tags and changes default realization of equals and hashcode for tag

--- src/main/java/com/summergroup/portal/domain/Tag.java
+++ src/main/java/com/summergroup/portal/domain/Tag.java
@@ -1,12 +1,14 @@
 package com.summergroup.portal.domain;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import javax.persistence.*;
 import java.util.Set;
 
 @Data
 @Entity
+@EqualsAndHashCode(exclude = {"id", "workshopItems"})
 @Table(name = "tags")
 public class Tag {
     @Id
